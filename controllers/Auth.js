8d54fc612f779eb1e444eaf9bc430b19457a6538
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-08-12 14:39:44 +0530

    fix: auth route

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,43 +1,46 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Orders = require("../models/Orders");
 
-const login = (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(422).json({ error: "Please Provide email or password" });
-  }
-  User.findOne({ email: email }).then((savedUser) => {
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(422)
+        .json({ error: "Please Provide email or password" });
+    }
+
+    const savedUser = await User.findOne({ email: email });
     if (!savedUser) {
       return res.status(422).json({ error: "Invalid Credentials" });
     }
-    bcrypt
-      .compare(password, savedUser.password)
-      .then((doMatch) => {
-        if (doMatch) {
-          const token = jwt.sign(
-            { _id: savedUser._id },
-            process.env.JWT_SECRET
-          );
-          const { _id, name } = savedUser;
-          res.json({ token, user: { _id, name, userStatus } });
-        } else {
-          return res.status(422).json({ error: "Invalid Credentials" });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
+
+    const doMatch = await bcrypt.compare(password, savedUser.password);
+    if (doMatch) {
+      const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
       });
-  });
+      const { _id, name, userStatus } = savedUser;
+      return res.json({ token, user: { _id, name, userStatus } });
+    } else {
+      return res.status(422).json({ error: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const signUp = async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!email || !name || !password) {
-    return res.status(422).json({ error: "Please add all the Fields" });
-  }
-  await User.findOne({ email: email }).then((savedUser) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!email || !name || !password) {
+      return res.status(422).json({ error: "Please add all the Fields" });
+    }
+
+    const savedUser = await User.findOne({ email: email });
     if (savedUser) {
       return res
         .status(422)
@@ -50,13 +53,12 @@ const signUp = async (req, res) => {
       password,
     });
 
-    user
-      .save()
-      .then(() => {
-        res.json({ message: "User Added Successfully" });
-      })
-      .catch((error) => console.log(error));
-  });
+    await user.save();
+    res.json({ message: "User Added Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
